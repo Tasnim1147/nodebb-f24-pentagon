222b9777bb19666989ab7c41c75e2e90c1bfbc2c
Deema <dmohanad@andrew.cmu.edu>
2024-09-25 00:26:10 +0300

    Fixed the error by changing the way the isApproved variable was being set

--- src/posts/create.js
+++ src/posts/create.js
@@ -19,7 +19,7 @@ module.exports = function (Posts) {
 		const content = data.content.toString();
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
-		const isApproved = false;
+		const { isApproved } = data;
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
