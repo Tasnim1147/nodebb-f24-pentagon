849199678b22c588ef6908271d73432997b65c3a
salman <ssaigh@andrew.cmu.edu>
2024-09-25 11:49:17 +0300

    Fix trailing space and object deconstructing lint errors

--- src/posts/create.js
+++ src/posts/create.js
@@ -19,8 +19,7 @@ module.exports = function (Posts) {
 		const content = data.content.toString();
 		const timestamp = data.timestamp || Date.now();
 		const isMain = data.isMain || false;
-		const annonymousType = data.annonymousType;
-		
+		const { annonymousType } = data;
 
 		if (!uid && parseInt(uid, 10) !== 0) {
 			throw new Error('[[error:invalid-uid]]');
